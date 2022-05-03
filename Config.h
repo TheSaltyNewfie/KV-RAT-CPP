b4add2666e09b926e5c202df8c2a7b50890d1eae
TheSaltyNewfie <46291294+TheSaltyNewfie@users.noreply.github.com>
2022-05-03 10:48:33 -0230

    added some settings stuff

--- Config.h
+++ Config.h
@@ -1,1 +1,7 @@
 #pragma once
+#include <Windows.h>
+#include <fstream>
+#include <vector>
+#include <iostream>
+
+int changeSettings(std::vector<std::string> settings);
--- Utils.cpp
+++ Utils.cpp
@@ -1,10 +1,28 @@
 #include <Windows.h>
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
+#include <SFML/Graphics.hpp>
 #include <string>
 #include <vector>
 #include <sstream>
 #include "Utils.h"
+#include "Config.h"
+
+// Say something on the server side is going to change such as an address or port, the client can know before hand
+void changeSettings(sf::TcpSocket& server, std::string& hostname, std::string& port, std::string& debug)
+{
+	//This part of the code is what changes settings
+	std::vector<std::string> data = {hostname, port, debug};
+
+	changeSettings(data);
+
+	//This part of the code is executed after the settings are changed
+	sf::RenderWindow window(sf::VideoMode(256, 512), "Alert");
+	sf::Font font;
+	if (!font.loadFromFile("arial.ttf"))
+		std::cout << "Error when loading font";	
+	sf::Text text("A client setting has changed. Please restart PC or program when possible. Oh and if you see this please tell me kasean, incase something has gone wrong", font, 50);
+}
 
 void message(sf::TcpSocket& server, std::string& msg)
 {

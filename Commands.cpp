90af469ed9351f953e7af9315516155c8532ebcb
TheSaltyNewfie <46291294+TheSaltyNewfie@users.noreply.github.com>
2022-05-05 09:45:35 -0230

    Added stuff

--- Commands.cpp
+++ Commands.cpp
@@ -8,7 +8,7 @@
 Takes 3 inputs, file path, delay, delay amount
 
 This uses SFML for streaming and playing the audio and is considered music so it will be streamed
-returns -1 if it errors out, returns 0 if it was successful.
+returns 1 if it errors out, returns 0 if it was successful.
 */
 int playAudio(std::string& fpath, bool delay = false, float delayTime = 0)
 {
@@ -20,8 +20,13 @@ int playAudio(std::string& fpath, bool delay = false, float delayTime = 0)
     sf::Music audio;
     if(!audio.openFromFile(fpath))
     {
-        return -1;
+        return 1;
     }
     audio.play();
     return 0;
+}
+
+void showUpdateWindow(std::string& header, std::string& text)
+{
+    
 }

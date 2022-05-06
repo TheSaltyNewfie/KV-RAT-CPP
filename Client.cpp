140000e8e216ac19e37fc410a70a99a21658c7b9
TheSaltyNewfie <tobydodge65@gmail.com>
2022-05-06 08:11:08 -0230

    Bruh
    Changed some stuff such as the client, it now takes packets instead of raw bytes for reasons such as the null error that you would receive before now

--- Client.cpp
+++ Client.cpp
@@ -9,14 +9,23 @@
 
 int main()
 {
-	std::string addr = "192.168.0.2";
+	std::string addr = "localhost";
 	int port = 4560;
 
 	sf::TcpSocket socket;
 	sf::Socket::Status status = socket.connect(addr, port);
 
+<<<<<<< Updated upstream
 	sf::Packet packet;
 	std::string serverCommands;
+=======
+	//char data[100];
+	//std::string data[1];
+	//unsigned char data[MAX_DATA_SIZE + 1]; //This may work but if not I am going to make a null thing
+	//std::size_t received;
+	sf::Packet packet;
+	std::string data;
+>>>>>>> Stashed changes
 
 	try
 	{
@@ -26,9 +35,19 @@ int main()
 		}
 		std::cout << "Connected to " << addr << ":" << port << std::endl;
 
+<<<<<<< Updated upstream
 		socket.recieve(packet);
 		packet >> serverCommands; //This may work with the server who knows
 		std::cout << "Data in packet: " << serverCommands;
+=======
+		//socket.receive(data, MAX_DATA_SIZE + 1, received);
+		socket.receive(packet);
+		packet >> data;
+
+
+		std::cout << "Data:" << data << std::endl << "Received:" << "Maybe 3 could be 4" << std::endl;
+		//std::cout << "Message hopefully defucked: " << returnCommands(data, received)[1];
+>>>>>>> Stashed changes
 
 	}
 	catch (const std::exception& e) { std::cout << "Error: " << e.what(); }
